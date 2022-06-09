e9e402498f78aed852f27320b7ebef65ccbf0730
darling1823 <gabrielalcantara1823@gmail.com>
2022-06-09 12:21:23 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+let nome = 'Gabriel'
+
+let number = 19
+let ano = 2022 - number
+console.log(ano)
+
+let mensagem = 'Estou mexendo no console.log'
+console.log(mensagem)
+
+let quartaFeira = false
+console.log(quartaFeira)
+console.log(typeof quartaFeira)
